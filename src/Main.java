0db787da73667bcea4aafa6425f86c3dec31f2ef
Danil <dvpudovkin@gmail.com>
2022-06-22 15:56:35 +0500

    Добавил проверку мат. выражения и извлечение операндов

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.lang.ArrayIndexOutOfBoundsException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,10 +9,32 @@ public class Main {
         String input = scanner.nextLine();
 
         // Передаём данные в метод calc
-        calc(input);
+        System.out.println(calc(input));
     }
 
     public static String calc(String input) {
+        // Разделяем строку с математическим выражением
+        String[] expression = input.split(" +");
+
+        String[] numbers;
+        String operator;
+
+        // Обрабатываем ограничения
+        if (expression.length < 3 | expression.length % 2 == 0) {
+            return "throws Exception //т.к. строка не является математической операцией";
+        } else if (expression.length > 3) {
+            return "throws Exception //т.к. формат математической операции не удовлетворяет заданию - " +
+                    "два операнда и один оператор (+, -, /, *)";
+        } else {
+            // Получаем числа и оператор
+            numbers = new String[] {expression[0], expression[2]};
+            operator = expression[1];
+        }
+
+//        Expression expression = new Expression(input);
+        for (String val: expression) {
+            System.out.println(val);
+        }
         return " ";
     }
 }
