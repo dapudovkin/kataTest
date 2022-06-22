3ef3992db7ecfac069a7f81a8aae6d616d0cab60
Danil <dvpudovkin@gmail.com>
2022-06-22 14:09:32 +0500

    Добавил методы main и calc, сделал ввод данных

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаём сканер и вводим данные
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        // Передаём данные в метод calc
+        calc(input);
+    }
+
+    public static String calc(String input) {
+        return " ";
+    }
+}
